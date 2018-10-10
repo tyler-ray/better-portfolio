c7aec25e48014bc56bec8b2cc17646a12bfee57f
unknown <jtray@outlook.es>
2018-10-10 12:48:44 -0600

    ready for hosting

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-const express = require ('express')
-const bodyParser = require ('body-parser')
+const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 4321
-const nodemailer = require ('nodemailer')
+const nodemailer = require('nodemailer')
 app.use(bodyParser.json())
-require ('dotenv').config()
+app.use(express.static(`${s__dirname}/build`))
+require('dotenv').config()
 //nodemailer:
 let transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -18,33 +19,33 @@ let transporter = nodemailer.createTransport({
     rejectUnauthorized: false
   }
 });
-app.get('/api/get', (req,res)=>{
-    console.log('this is working')
-    res.send('this worked.').status(200)
+app.get('/api/get', (req, res) => {
+  console.log('this is working')
+  res.send('this worked.').status(200)
 })
 
-    
-app.post('/api/inquire', (req,res)=>{
-let {name, email, phone, message} = req.body
-      let HelperOptions = {
-            from: '"JTylerRay.com" <noreply@jtylerray.com>',
-            to: 'tylerrayprofessional@gmail.com',
-            subject: `You've got a website message from ${name}.`,
-            text: `From: ${name} @ ${email} Phone:${phone}.  "${message}" -- `
+
+app.post('/api/inquire', (req, res) => {
+  let { name, email, phone, message } = req.body
+  let HelperOptions = {
+    from: '"JTylerRay.com" <noreply@jtylerray.com>',
+    to: 'tylerrayprofessional@gmail.com',
+    subject: `You've got a website message from ${name}.`,
+    text: `From: ${name} @ ${email} Phone:${phone}.  "${message}" -- `
   };
-  
-    transporter.sendMail(HelperOptions, (error, info) => {
-          if (error) {
-        return console.log(error);
-      }
-      console.log("The message was sent!");
-      console.log(info);
-      res.send('successful').status(200)
+
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("The message was sent!");
+    console.log(info);
+    res.send('successful').status(200)
   });
   console.log(name, email, phone, message)
-  })
+})
 
 
 
 
-app.listen(port, () =>{console.log(`deploying ${port} plastic cups`)})
+app.listen(port, () => { console.log(`deploying ${port} plastic cups`) })
