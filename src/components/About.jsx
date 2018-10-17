9cbe0ef1c919446d611c4e5aec8cfe79187850d4
unknown <jtray@outlook.es>
2018-10-17 15:07:25 -0600

    tweaking

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ function About() {
             be programmed to do his homework for him. That started a landslide of love for computing and programming. I discovered
             that I not only learned the material better when I created simple math programs on my calculator, but also that
             I really enjoyed programming it! I've been augmenting my computer skills since.</p></div>
-                <div className="right-about about"><p className ='aboutMe'>Besides Computing, I have a passion for Martial Arts, Ballroom Dance, and Theatre. I earned my
+                <div className="right-about about"><p className ='aboutMe'>Besides computing, I have a passion for Martial Arts, Ballroom Dance, and Theatre. I earned my
             Eagle Scout award at the age of 14, and served two years in Mexico, Saltillo for the Church of Jesus Christ of
             Latter Day Saints at the age of 19. I moved to Hawaii for a semester to study in BYU Hawaii's business program.
-            I returned to my hometown of Mesa, Arizona for a bit, and then moved to Utah to follow my passion for computer
-            programming.</p></div>
+            I moved to Utah to follow my passion for computer
+            programming. I studied at the Web Design Bootcamp DevMountain to sharpen my skills. It was a phenominal experience which allowed me to gain the skills I need to comfortably design and build websites from scratch. </p></div>
             </div>
         </div>
     )
